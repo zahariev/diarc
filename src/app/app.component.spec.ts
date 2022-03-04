aeac98e33281573b959944e3efdc8808a2d50905
Boyan Zahariev <bzahariev@soft2run.com>
2022-03-04 10:25:10 +0200

    fix: project name to diarc

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'yoco'`, () => {
+  it(`should have as title 'diarc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    // expect(app.title).toEqual("yoco");
+    // expect(app.title).toEqual("diarc");
   });
 
   it("should render title", () => {
@@ -25,7 +25,7 @@ describe("AppComponent", () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector(".content span")?.textContent).toContain(
-      "yoco app is running!"
+      "diarc app is running!"
     );
   });
 });
